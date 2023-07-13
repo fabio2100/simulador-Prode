09846e70217d4a5c21381166296f85e77f231f52
Fabio Squizzato <fsquizzato@visualmedica.com>
2023-07-13 17:53:44 -0300

    Simular resultados funcionando, no se como pero funcionando

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import { Match } from './components/match';
 
 function App() {
 
+  const [resultados,setResultados] = useState([]);
+  const [simulado,setSimulado] = useState(false)
   const n = 8;
   const matchs = [...Array(n)].map((e,i)=>
     <Match key={i} num={i} numberOfMatch={i}/>
   )
 
-  let resultsMatch = [...Array(n)].map((e,i)=>
-    <Match key={i+n} num={i+n} isReadOnly={true} resultado={4} numberOfMatch={i}/>
-  )
+  const restulsMatch = ([...Array(n)].map((e,i)=>
+    <Match key={i+n} num={i+n} isReadOnly={true} resultado={Math.floor(Math.random()*3)}  numberOfMatch={i}/>
+  ))
 
-  const simular = value => () => {
-    /*resultsMatch.forEach((match)=>{
-      const resultOfMatch = Math.floor(Math.random()*3)
-      console.log({resultOfMatch});
-      console.log({match})
-      match.props.resultado = resultOfMatch;
-      
-    })*/
-    const resultOfMatch = Math.floor(Math.random()*3);
-    let resultsMatch = [...Array(n)].map((e,i)=>{
-      <Match key={i+n} num={i+n} isReadOnly={true} resultado={4} numberOfMatch={i}/>   
-    }) 
+  
+  const simular = prevComponents => function() {
+    console.log(restulsMatch)
+    setResultados(restulsMatch)
   }
+
+
 
   return (
     <div className='main'>
@@ -34,7 +31,7 @@ function App() {
       </div>
       <div id="resultados">
         <h1>Resultados</h1>
-        {resultsMatch}
+        {resultados}
       </div>
       <button
       type="button"
--- src/components/match.jsx
+++ src/components/match.jsx
@@ -22,16 +22,16 @@ function Team(){
 
 function Puntuacion(props){
   const isReadOnly = props.isReadOnly;
-  const resultado = props.resultado
+  const resultado = props.resultado; 
 
   const onChangeRadio = function(event){
     console.log(props.isReadOnly)
   }
   return (
     <>
-        <input type="radio" className='radioButton' name={props.name} onChange={onChangeRadio} disabled={isReadOnly ? true : false}   checked={resultado==0?true:false}></input>
-        <input type="radio" className='radioButton' name={props.name} onChange={onChangeRadio} disabled={isReadOnly ? true : false}   checked={resultado==1?true:false}></input>
-        <input type="radio" className='radioButton' name={props.name} onChange={onChangeRadio} disabled={isReadOnly ? true : false}   checked={resultado==2?true:false}></input>         
+        <input type="radio" className='radioButton' name={props.name} onChange={onChangeRadio} disabled={isReadOnly ? true : false} checked={resultado===0 ? true : false}></input>
+        <input type="radio" className='radioButton' name={props.name} onChange={onChangeRadio} disabled={isReadOnly ? true : false} checked={resultado===1 ? true : false}></input>
+        <input type="radio" className='radioButton' name={props.name} onChange={onChangeRadio} disabled={isReadOnly ? true : false} checked={resultado===2 ? true : false}></input>         
     </>
   )
 }
